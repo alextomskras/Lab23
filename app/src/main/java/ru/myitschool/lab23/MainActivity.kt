32edb15f510fc83ff303d9f0b6ab1ffca49f4872
alextomskras <dreamer.alex.toms@gmail.com>
2022-11-30 15:04:04 +0300

    add dimens for landscape and portrait v2

--- app/src/main/java/ru/myitschool/lab23/MainActivity.kt
+++ app/src/main/java/ru/myitschool/lab23/MainActivity.kt
@@ -1,9 +1,12 @@
 package ru.myitschool.lab23
 
 import android.os.Bundle
+import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import ru.myitschool.lab23.databinding.ActivityMainBinding
+import java.nio.file.Files.find
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,6 +21,12 @@ class MainActivity : AppCompatActivity() {
 
         val mainTexts = findViewById<TextView>(R.id.main_text)
         mainTexts.setText(R.string.main_text)
+        val container: ViewGroup = findViewById(R.id.outer_layout)
+        val views = container.childCount
+//        Toast.makeText(this, "landscape", Toast.LENGTH_LONG).show();
+        Toast.makeText(this,"$views",Toast.LENGTH_LONG).show()
 
+//        ViewGroup root = (ViewGroup) findViewById(android.R.id.coordinatorLayout)
+//        int contentChildrenCount = root.getChildCount()
     }
 }
